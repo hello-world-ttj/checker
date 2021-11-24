32bef6afc9fcf4fa2dda1d7f42a8b775c5109ace
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2021-11-24 16:24:32 +0530

    Fibonacci series removed--- Armstrong Number added

--- js/script.js
+++ js/script.js
@@ -34,19 +34,64 @@ function palindromeNumber(x) {
 		}
         if(final==temp)
 		{
-			result1.innerText=`${x} is Palindrome number`
+			result1.innerText=`${x} is Palindrome number.`
 		}
 		else
 		{
-			result1.innerText=`${x} is not Palindrome number`
+			result1.innerText=`${x} is not Palindrome number.`
 		}
 }
 
-// FIBONACCI
+// PRIME
+
+let arrow3 = document.getElementById("arrow3")
+
+let prime = document.querySelector("#prime")
+
+let inFiled3 = document.getElementById("inFiled3")
+
+let btn3 = document.getElementById("btn3")
+
+let result3 = document.getElementById("result3")
+
+arrow3.addEventListener("click", function() {
+    prime.className = "checker-div_active"
+})
+
+btn3.addEventListener("click", function() {
+    let inValue3 = inFiled3.value
+    let x = parseInt(inValue3)
+    primeNumber(x)
+    inFiled3.value = ""
+})
+
+function primeNumber(x) {
+	let num = x
+	let isPrime = true
+	if(num ==1 ) {
+		result3.innerText = "1 is neither prime nor composite number."
+	}else if (num > 1) {
+		for(let i = 2; i < num; i++){
+			if(num % i == 0) {
+				isPrime = false
+				break
+			}
+		}
+		if (isPrime) {
+			result3.innerText=`${num} is a prime number.`
+		} else {
+			result3.innerText=`${num} is a not prime number.`
+		}
+	}else {
+		result3.innerText = "The number is not a prime number."
+	}
+}
+
+// ARMSTRONG
 
 let arrow2 = document.getElementById("arrow2")
 
-let fibonacci = document.querySelector("#fibonacci")
+let armstrong = document.querySelector("#armstrong")
 
 let inFiled2 = document.getElementById("inFiled2")
 
@@ -54,29 +99,41 @@ let btn2 = document.getElementById("btn2")
 
 let result2 = document.getElementById("result2")
 
-let result2_1 = document.getElementById("result2_1")
 
 arrow2.addEventListener("click", function() {
-	fibonacci.className = "checker-div_active"
+	armstrong.className = "checker-div_active"
 })
 
 btn2.addEventListener("click", function() {
     let inValue2 = inFiled2.value
     let x = parseInt(inValue2)
-    fibonacciSeries(x)
+    armstrongNumber(x)
     inFiled2.value = ""
 })
 
 
-function fibonacciSeries(x) {
+function armstrongNumber(x) {
 	let num = x
-	console.log(num);
-	let n1 = 0, n2 = 1, n3
-	result2.innerText = `Fibonacci Series of :${num}`
-	for(let i = 1; i <= num; i++) {
-		result2_1.innerText += n1 +'|'
-		n3 = n1 + n2
-		n1 = n2
-		n2 = n3
+	let digits = 0
+	let sum = 0
+	let temp = num
+// Finding the number of digits
+	while (num > 0) {
+		digits = digits + 1
+		num = parseInt(num / 10)
+	}
+
+	num = temp
+	// calculating sum
+	while (num > 0) {
+		let digit = num % 10
+		sum = sum + Math.pow(digit, digits)
+		num = parseInt(num / 10)
+	}
+
+	if(sum == temp) {
+		result2.innerText=`${temp} is an Armstrong number.`
+	}else {
+		result2.innerText=`${temp} is not an Armstrong number.`
 	}
 }
